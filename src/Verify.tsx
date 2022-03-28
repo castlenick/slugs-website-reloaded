3ec029dd764bcad6f621375e2cf8028f9019a476
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2022-03-28 19:32:59 -0400

    Include roles in verify success message

--- src/Verify.tsx
+++ src/Verify.tsx
@@ -17,11 +17,19 @@ interface DiscordUser {
     public_flags: number;
 }
 
+const test = true;
+
 const backendApiURL = `https://letsalllovelain.com/verify/`;
+const testBackendApiURL = `http://localhost:5353/`;
+const apiURL = test ? testBackendApiURL : backendApiURL;
+
+const mainRedirectURL = `https://solslugs.com/`;
+const testRedirectURL = `http://localhost:3000/`;
+const redirectURL = test ? testRedirectURL : mainRedirectURL;
 
 const discordArgs = {
     client_id: '903156913724874832',
-    redirect_uri: 'https://solslugs.com/',
+    redirect_uri: redirectURL,
     response_type: 'token',
     scope: 'identify',
     state: 'verify',
@@ -30,6 +38,7 @@ const discordArgs = {
 export function Verifier() {
     const [user, setUser] = useState<DiscordUser | null | false>(null);
     const [discordError, setDiscordError] = useState<string | null>(null);
+    const [rolesApplied, setRolesApplied] = useState<any[] | null>(null);
 
     const [verifyInProgress, setVerifyInProgress] = useState(false);
 
@@ -49,6 +58,68 @@ export function Verifier() {
     ];
 
     const [verified, setVerified] = useState<boolean | null>(null);
+
+    async function handleSignVerifyMessage() {
+        if (!user || !publicKey) {
+            return;
+        }
+
+        const address = publicKey.toString();
+
+        const toSign = new TextEncoder().encode(
+            `I am signing this to link my discord identity ${
+                user!.username.replace(/\s/g, '')
+            }#${user!.discriminator} with id ${
+                user!.id
+            } to my solana wallet ${address}`
+        );
+
+        const sig = await signMessage!(
+            toSign,
+        );
+
+        setVerifyInProgress(true);
+        setRolesApplied(null);
+
+        const verifyRequest = {
+            address,
+            toSign: encode(toSign),
+            signature: encode(sig),
+            discordToken: accessToken,
+        };
+
+        try {
+            const res = await fetch(
+                apiURL,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type":
+                            "application/json",
+                    },
+                    body: JSON.stringify(
+                        verifyRequest
+                    ),
+                }
+            );
+
+            setVerifyInProgress(false);
+
+            if (res.status === 200) {
+                const roleData = await res.json();
+                console.log(roleData);
+                setVerified(true);
+                setRolesApplied(roleData);
+            } else {
+                setVerified(false);
+            }
+        } catch (err) {
+            console.log((err as any).toString());
+            setVerified(false);
+            setVerifyInProgress(false);
+        }
+
+    }
 
     useEffect(() => {
         if (!accessToken || !tokenType) {
@@ -83,13 +154,13 @@ export function Verifier() {
     if (!accessToken) {
         return (
             <div className="flex flex-col items-center justify-center gap-y-5">
-                <p>
-                    Let's Get Verified!
-                </p>
+                <span className="w-3/5 text-center">
+                    Let's get verified! This will verify you are a slug holder and unlock certain channels and roles in the Discord server.
+                </span>
 
-                <p>
+                <span>
                     Start by linking your Discord account.
-                </p>
+                </span>
 
                 <button
                     className="background-transparent border-slugGreen border-2 uppercase w-52 text-4xl p-4 rounded h-16 align-middle flex items-center justify-center"
@@ -137,79 +208,35 @@ export function Verifier() {
                                 </p>
 
                                 <p>
-                                    {verifyInProgress ? 'Please wait, verifying your holder status and applying roles...' : 'Finally, sign a message to verify your NFT.'}
+                                    {verifyInProgress ? 'Please wait, verifying your holder status and applying roles...' : 'Finally, sign a message to verify your address and get your roles.'}
                                 </p>
 
                                 <button
                                     className="background-transparent border-slugGreen border-2 uppercase w-96 text-4xl p-4 rounded h-16 align-middle flex items-center justify-center"
                                     disabled={verifyInProgress}
-                                    onClick={async () => {
-                                        const address = publicKey.toString();
-
-                                        const toSign = new TextEncoder().encode(
-                                            `I am signing this to link my discord identity ${
-                                                user!.username.replace(/\s/g, '')
-                                            }#${user!.discriminator} with id ${
-                                                user!.id
-                                            } to my solana wallet ${address}`
-                                        );
-
-                                        const sig = await signMessage!(
-                                            toSign,
-                                        );
-
-                                        setVerifyInProgress(true);
-
-                                        const verifyRequest = {
-                                            address,
-                                            toSign: encode(toSign),
-                                            signature: encode(sig),
-                                            discordToken: accessToken,
-                                        };
-
-                                        try {
-                                            const res = await fetch(
-                                                backendApiURL,
-                                                {
-                                                    method: "POST",
-                                                    headers: {
-                                                        "Content-Type":
-                                                            "application/json",
-                                                    },
-                                                    body: JSON.stringify(
-                                                        verifyRequest
-                                                    ),
-                                                }
-                                            );
-
-                                            setVerifyInProgress(false);
-
-                                            if (res.status === 200) {
-                                                setVerified(true);
-                                            } else {
-                                                setVerified(false);
-                                            }
-                                        } catch (err) {
-                                            console.log((err as any).toString());
-                                            setVerified(false);
-                                            setVerifyInProgress(false);
-                                        }
-                                    }}
+                                    onClick={handleSignVerifyMessage}
                                 >
                                     Sign Message to Verify
                                 </button>
                             </>
                         )}
 
-                        {verified === true && (
+                        {rolesApplied && verified === true && (
                             <>
-                                <p>
-                                    Success!
-                                </p>
+                                <span className="text-center">
+                                    Success! You have been given the following roles:
+                                </span>
+
+                                <span className="text-center">
+                                    {rolesApplied.map((r) => (
+                                        <span className="inline mx-3 my-3" style={{ color: r.color }} key={r.name}>
+                                            {r.name}
+                                        </span>
+                                    ))}
+                                </span>
 
                                 <p>
-                                    You have been verified. You can return to
-                                    Discord.
+                                    You can return to Discord.
                                 </p>
                             </>
                         )}
