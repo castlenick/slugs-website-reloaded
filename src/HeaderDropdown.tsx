8c197bb5da4eec7c5a0d0803e4351a166a17fc0f
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2022-03-30 18:54:20 -0400

    Improve dropdown handling

--- src/HeaderDropdown.tsx
+++ src/HeaderDropdown.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Menu } from '@headlessui/react';
 
 export interface HeaderLink {
@@ -20,6 +20,16 @@ export function HeaderDropdown(props: HeaderDropdownProps) {
         links,
     } = props;
 
+    const navigate = useNavigate();
+
+    function handleLink(link: string, external?: boolean) {
+        if (external) {
+            window.location.href = link;
+        } else {
+            navigate(link);
+        }
+    }
+
     return (
         <Menu as="div" className="relative">
             <Menu.Button className="flex justify-center items-center gap-x-2">
@@ -30,7 +40,12 @@ export function HeaderDropdown(props: HeaderDropdownProps) {
             </Menu.Button>
             <Menu.Items className="absolute flex flex-col justify-center items-center right-0 w-60 mt-2 origin-top-right bg-black border-2 border-slugGreen rounded z-10 divide-y divide-slugGreen">
                 {links.map(({ link, name, external }) => (
-                    <Menu.Item as="div" className="w-full flex justify-center items-center py-2 hover:bg-slugGreenDark" key={name}>
+                    <Menu.Item
+                        as="div"
+                        className="w-full flex justify-center items-center py-2 hover:bg-slugGreenDark cursor-pointer"
+                        key={name}
+                        onClick={() => handleLink(link, external)}
+                    >
                         {() => external ? (
                                 <a href={link} key={link}>
                                     <span className="text-3xl">
